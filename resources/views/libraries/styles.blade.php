317dd997cf6801d01ee5140925753d6b025f15ff
makaranda <makarandapathirana@gmail.com>
2024-04-09 17:49:48 +0530

    updated

--- resources/views/libraries/styles.blade.php
+++ resources/views/libraries/styles.blade.php
@@ -11,8 +11,7 @@
     <link rel="stylesheet" href="{{ URL::to('') }}/resources/vendors/magnefic-popup/magnific-popup.css">
     <link rel="stylesheet" href="{{ URL::to('') }}/resources/vendors/owl-carousel/owl.theme.default.min.css">
     <link rel="stylesheet" href="{{ URL::to('') }}/resources/vendors/owl-carousel/owl.carousel.min.css">
-    <link rel="stylesheet" href="{{ URL::to('') }}/resources/vendors/nice-select/nice-select.css">
-    
+    <link rel="stylesheet" href="{{ URL::to('') }}/resources/vendors/nice-select/nice-select.css">   
 
     <!-- <link rel="stylesheet" href="css/style.css"> -->
     <!-- <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css"> -->
